30e6414a55ca9d87cd3554d410025af85d8003f0
Vincent Lore <vincent_lore@hotmail.com>
2016-05-13 18:27:31 -0700

    Project overview animations

--- app/components/home/HomeCtrl.js
+++ app/components/home/HomeCtrl.js
@@ -1,18 +1,27 @@
 app = angular.module('HomeCtrl', []);
 
-app.controller('HomeCtrl', function (filterFilter, $http) {
+app.controller('HomeCtrl', function (filterFilter, $http, $timeout) {
 
     var vm = this;
 
     vm.queryCourses = '';
     vm.courses = [];
-    vm.isCourses = true;
-    vm.isIntro = true;
-    vm.isEducation = true;
+    vm.showCourses = true;
+    vm.showBio = true;
+    vm.showEducation = true;
+    vm.showProjects = false;
 
     vm.loadCourses = function () {
         $http.get('components/home/Courses.json').success(data => 
             vm.courses = data);
-    }
-    
+    };
+
+    vm.toggleProjects = function () {
+        vm.showBio = !vm.showBio;
+        vm.showEducation = !vm.showEducation;
+        $timeout(function () {
+            vm.showProjects = !vm.showProjects;
+        }, 800);
+    };
+
 })
